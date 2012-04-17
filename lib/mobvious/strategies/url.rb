79456d78a9be302b3bc54c3dc24b6728c28e6bb6
Jiří Stránský <jistr@jistr.com>
2012-04-17 18:01:05 +0200

    URL strategy can be disabled by conditionally checking HTTP Referer header

--- lib/mobvious/strategies/url.rb
+++ lib/mobvious/strategies/url.rb
@@ -11,8 +11,22 @@ module Mobvious
       #   A hash containing regular expressions mapped to symbols. The regular expression
       #   is evaluated against the whole URL of the request (including `http://`). If matching,
       #   the corresponding symbol is returned as the device type.
-      def initialize(rules = MOBILE_PATH_RULES)
+      # @param options
+      #   A hash with strategy options.  
+      #   `disable_if_referer_set: true` disables the strategy if HTTP Referer header is set  
+      #   `disable_if_referer_matches: /regex/` disables the strategy if HTTP Referer matches
+      #   given regular expression  
+      #   `disable_unless_referer_matches: /regex/` disables the strategy if HTTP Referer
+      #   doesn't match given regular expression  
+      def initialize(rules = MOBILE_PATH_RULES, options = {})
         @rules = rules
+
+        default_options = {
+          disable_if_referer_set: false,
+          disable_if_referer_matches: nil,
+          disable_unless_referer_matches: nil
+        }
+        @options = default_options.merge(options)
       end
 
       # Gets device type using URL pattern matching. Returns nil if no match found.
@@ -20,10 +34,30 @@ module Mobvious
       # @param request [Rack::Request]
       # @return [Symbol] device type or nil
       def get_device_type(request)
+        return nil if disabled_by_referer_set?(request) ||
+                      disabled_by_referer_matching?(request) ||
+                      disabled_by_referer_not_matching?(request)
+
         @rules.each do |regex, device_type|
           return device_type if request.url =~ regex
         end
         nil
+      end
+
+      private
+
+      def disabled_by_referer_set?(request)
+        @options[:disable_if_referer_set] && request.env['HTTP_REFERER']
+      end
+
+      def disabled_by_referer_matching?(request)
+        @options[:disable_if_referer_matches] &&
+            request.env['HTTP_REFERER'] =~ @options[:disable_if_referer_matches]
+      end
+
+      def disabled_by_referer_not_matching?(request)
+        @options[:disable_unless_referer_matches] &&
+            request.env['HTTP_REFERER'] !~ @options[:disable_unless_referer_matches]
       end
     end
   end
--- spec/mobvious/strategies/url_spec.rb
+++ spec/mobvious/strategies/url_spec.rb
@@ -17,13 +17,70 @@ class URLSpec < MiniTest::Spec
     end
 
     it "returns the right device type when matching rule found" do
-      puts @request.url
       @strategy.get_device_type(@request).must_equal :mobile
     end
 
     it "returns nil if no matching rule found" do
       @env['HTTP_HOST'] = 'www.foo.com'
       @strategy.get_device_type(@request).must_equal nil
+    end
+
+    describe "disabled if referer is set" do
+      before do
+        @strategy = URL.new(URL::MOBILE_PATH_RULES, disable_if_referer_set: true)
+        @env.merge!({
+          'HTTP_REFERER' => 'http://localhost'
+        })
+      end
+
+      it "returns nil even when matching rule found" do
+        @strategy.get_device_type(@request).must_equal nil
+      end
+    end
+
+    describe "disabled if referer is set" do
+      before do
+        @strategy = URL.new(URL::MOBILE_PATH_RULES, disable_if_referer_set: true)
+        @env.merge!({
+          'HTTP_REFERER' => 'http://localhost'
+        })
+      end
+
+      it "returns nil even when matching rule found" do
+        @strategy.get_device_type(@request).must_equal nil
+      end
+    end
+
+    describe "disabled if referer matches" do
+      before do
+        @env.merge!({
+          'HTTP_REFERER' => 'http://localhost'
+        })
+      end
+
+      it "returns nil even when matching rule found" do
+        @strategy = URL.new(URL::MOBILE_PATH_RULES, disable_if_referer_matches: /local/)
+        @strategy.get_device_type(@request).must_equal nil
+
+        @strategy = URL.new(URL::MOBILE_PATH_RULES, disable_if_referer_matches: /nothing/)
+        @strategy.get_device_type(@request).must_equal :mobile
+      end
+    end
+
+    describe "disabled unless referer matches" do
+      before do
+        @env.merge!({
+          'HTTP_REFERER' => 'http://localhost'
+        })
+      end
+
+      it "returns nil even when matching rule found" do
+        @strategy = URL.new(URL::MOBILE_PATH_RULES, disable_unless_referer_matches: /nothing/)
+        @strategy.get_device_type(@request).must_equal nil
+
+        @strategy = URL.new(URL::MOBILE_PATH_RULES, disable_unless_referer_matches: /local/)
+        @strategy.get_device_type(@request).must_equal :mobile
+      end
     end
   end
 end
